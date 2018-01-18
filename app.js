12b4990bd880461214745244a4e7aadc5bf8cb85
garg.v <garg.v@mmmsagargv.internal.directi.com>
2018-01-18 17:35:51 +0530

    1. UI overhaul 2. code refactored for directory structure 3. xx - Search yet to be implemented

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//----------CONFIGURATIONS----------------
+
 var express = require('express')
 var fs = require('fs')
 var http = require('http')
@@ -9,71 +11,93 @@ var app = express()
 app.set('view engine', 'ejs')
 app.use('/assets', express.static('./public'))
 
-var server= http.createServer(app)
+var server = http.createServer(app)
 var io = socket(server)
 
 server.listen(3000)
 
 var tailFile = ''
 
-//----------CONFIG COMPLETE----------------
 
+//-----------GENERATE DIRECTORY INFORMATION---------------
 var varlog = '/var/log'
 
-app.get('/*', function(req, res){
 
-	if(fs.statSync(varlog+req.originalUrl).isFile()){
-		tailFile = req.originalUrl
-		res.render('log')
-	}
-	else if(fs.statSync(varlog+req.originalUrl).isDirectory()){
+var directoryGenerator = function(url){
 
-		var directoryStructure = []
+	var directoryStructure = []
 
-		var dirList = fs.readdirSync(varlog + req.originalUrl)
+        var dirList = fs.readdirSync(varlog + url)
 
-		dirList.forEach(function(files){
-			/*`directoryStructure` tells if object is file or directory
-			for GUI purposes. Appropriate icons to be shown. */
-			if(fs.statSync(varlog + req.originalUrl + "/" + files).isFile()){
-				directoryStructure.push({
-					name: files,
-					isFile: true
-				})
-			}
-			else{
-				directoryStructure.push({
-					name: files,
-					isFile: false
-				})
-			}
-		})
+        dirList.forEach(function(files) {
+            /*`directoryStructure` tells if object is file or directory
+            for GUI purposes. Appropriate icons to be shown. */
+            if (fs.statSync(varlog + url + "/" + files).isFile()) {
+                directoryStructure.push({
+                    name: files,
+                    isFile: true
+                })
+            } else {
+                directoryStructure.push({
+                    name: files,
+                    isFile: false
+                })
+            }
+        })
 
-			
-		res.render('index', {
-			listing: directoryStructure,
-			url: req.originalUrl //to be appended to links in view
-		})
-	}
-	else{
-		console.log('Other format to be handled accordingly')
-	}
+        return directoryStructure
+}
+
+//--------------HANDLE ROUTES----------------
+
+app.get('/*', function(req, res) {
+
+    if (fs.statSync(varlog + req.originalUrl).isFile()) {
+
+    	url = req.originalUrl
+    	url = url.split('/')
+    	url.pop()
+    	url = url.join('/')
+
+    	directoryStructure = directoryGenerator(url)
+
+        tailFile = req.originalUrl
+
+        res.render('index', {
+            listing: directoryStructure,
+            url: url, //to be appended to links in view
+            stream: true
+        })
+
+    } else if (fs.statSync(varlog + req.originalUrl).isDirectory()) {
+
+        directoryStructure = directoryGenerator(req.originalUrl)
+
+
+        res.render('index', {
+            listing: directoryStructure,
+            url: req.originalUrl, //to be appended to links in view
+            stream: false
+        })
+    } else {
+        console.log('Other format to be handled accordingly')
+    }
 })
 
 
 
 //--------------- Sockets.io -----------------
 
-io.on('connection', function(socket){
-	var tail = child_process.spawn('tail', ['-f', '-n', 10 , varlog + tailFile])
+io.on('connection', function(socket) {
+    var tail = child_process.spawn('tail', ['-f', '-n', 25, varlog + tailFile])
 
-	tail.stdout.on('data', (data) => {
-	  io.emit('tailstream', data.toString().split("\n"))
-	});
+    tail.stdout.on('data', (data) => {
+        io.emit('tailstream', data.toString().split("\n"))
+    });
 
-	socket.on ('disconnect', function (socket){
-	    tail.kill()
-	    console.log('user disconnected')
-	});
+    socket.on('disconnect', function(socket) {
+        tail.kill()
+        console.log('user disconnected')
+    });
 
 })
